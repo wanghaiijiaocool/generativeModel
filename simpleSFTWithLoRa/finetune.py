d9c1fb96ff0fd7a16149f5e59188840396663b32
wanghaijiao <tx@192.168.1.5>
2023-03-31 22:21:48 +0800

    .

--- simpleSFTWithLoRa/finetune.py
+++ simpleSFTWithLoRa/finetune.py
@@ -44,6 +44,7 @@ DATA_PATH = "./alpaca_data_cleaned.json"
 
 device_map = "auto"
 world_size = int(os.environ.get("WORLD_SIZE", 1))
+print("WORLD_SIZE",world_size)
 ddp = world_size != 1
 if ddp:
     device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
