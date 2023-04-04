7d36f367fff1947d2baaa64d675a65a01c85842e
wanghaijiao <tx@192.168.1.5>
2023-04-04 15:09:49 +0800

    1

--- simpleSFTWithLoRa/tasks/finetune_llama_fasdp.py
+++ simpleSFTWithLoRa/tasks/finetune_llama_fasdp.py
@@ -33,6 +33,8 @@ from peft import (
 import functools
 
 def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
 
     # initialize the process group
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
