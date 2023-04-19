20816efe252b004a50fc55419b2d07816cf9973d
wanghaijiao <wanghaijiaocool@126.com>
2023-04-19 23:52:52 +0800

    1

--- disguish_learning/LR_001/download_model.py
+++ disguish_learning/LR_001/download_model.py
@@ -3,5 +3,7 @@
 cache_dir = '/root/autodl-tmp/model/'
 
 from transformers import  AutoModel,AutoTokenizer
+from datasets import  load_dataset
 model = AutoModel.from_pretrained('THUDM/glm-10b',cache_dir=cache_dir,trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained('THUDM/glm-2b',cache_dir=cache_dir,trust_remote_code=True)
+data = load_dataset("cahya/instructions-zh")
